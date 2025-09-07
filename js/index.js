faaf707c6f5fd73ddce46f40a441d6063d885928
Abdur55 <mrrahim972@gmail.com>
2025-09-08 05:48:54 +0600

    added cart and data

--- js/index.js
+++ js/index.js
@@ -1,16 +1,37 @@
 // catagory-section and btn
 
+const loadalldata = () => {
+    fetch('https://openapi.programming-hero.com/api/plants')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        displayalldata(data.plants)
+    })
+}
+
 const loadcatetoryBtn = () => {
     fetch('https://openapi.programming-hero.com/api/categories')
         .then(res => res.json())
         .then(data => displaycategory(data.categories))
 }
 
+const removeActive = () => {
+    const removeactiveclass = document.querySelectorAll('.btn-class')
+    removeactiveclass.forEach(btn => btn.classList.remove('active'))
+
+}
+
 
 const loadcategorydata = (id) => {
     fetch(`https://openapi.programming-hero.com/api/category/${id}`)
         .then(res => res.json())
-        .then(data => displayalldata(data.plants))
+        .then(data => {
+            removeActive()
+            const activebtn = document.getElementById(`active-btn-${id}`)
+            activebtn.classList.add('active')
+
+            displayalldata(data.plants)
+        })
 }
 
 const displayalldata = (data) => {
@@ -20,9 +41,26 @@ const displayalldata = (data) => {
 
     data.forEach(deta => {
         console.log(deta)
+        const {image, description, price,category, name} = deta
         const div = document.createElement('div')
         div.innerHTML = `
-        <h1> ${deta.name}</h1>
+         <div class="card bg-base-100 max-w-96 shadow-sm p-4 h-[550px]">
+                        <figure>
+                            <img  class="h-[250px] rounded w-full" src="${image}"
+                                alt="Not Found" />
+                        </figure>
+                        <div class="card-body p-4 space-y-2">
+                            <h2 class="card-title">${name}</h2>
+                            <p>${description}</p>
+                                <div class=" flex justify-between">
+                                    <div class="btn bg-green-200 rounded-full px-6 text-[#15803d]">${category}</div>
+                                    <div class="font-medium text-lg">$<span>${price}</span></div>
+                                </div>
+                            <div class="card-actions justify-end ">
+                                <button class="btn text-lg font-normal py-5 text-white w-full rounded-full bg-[#15803d]">Add to Cart</button>
+                            </div>
+                        </div>
+                    </div>
       `
         cartContainer.append(div)
 
@@ -37,8 +75,9 @@ const displaycategory = (categori) => {
     categori.forEach(btn => {
 
         const button = document.createElement('div')
+
         button.innerHTML = `
-     <div onclick="loadcategorydata(${btn.id})"  class="mt-2 pr-10 pl-2 rounded py-1 bg-green-500 hover:bg-green-300">${btn.category_name
+     <div id='active-btn-${btn.id}' onclick="loadcategorydata(${btn.id})"  class="mt-2 pr-10 pl-2 rounded py-1  hover:bg-green-300 btn-class">${btn.category_name
             }</div>
     `
         catagoryContainer.appendChild(button)
@@ -48,4 +87,5 @@ const displaycategory = (categori) => {
 
 
 
+loadalldata()
 loadcatetoryBtn()
